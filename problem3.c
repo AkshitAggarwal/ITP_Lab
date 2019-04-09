e7a7e59c2d32759aea90bb8606fe8b57998f7d30
Akshit Aggarwal <akshitx10@gmail.com>
2019-04-09 23:57:27 +0530

    commit 6

--- problem3.c
+++ problem3.c
@@ -1,7 +1,16 @@
 //WAP to calculate the simple interest. 
 #include<stdio.h>
+
+float simpleInterest(int P, int R, int T)
+{
+	return ((float)(P*R*T))/100;
+}
 int main()
 {
-    
+    int P, R, T;
+	printf("Enter values of Principal, Rate of Interest and Time period to calculate SI: ");
+	scanf("%d %d %d", &P, &R, &T);
+	float SI = simpleInterest(P, R, T);
+	printf("\n%f", SI);
 	return 0;
 }
